81417d4e287587f6a7ea5f857c3bc571f67d5ef7
Heather Hamilton <hghamilton+hackeryou@gmail.com>
2019-11-17 23:59:20 -0500

    Styling and functionality complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,64 @@
-// each question is inside of a div with a class of question[i]
-
-// on form submit, when the user clicks submit button
-// prevent default
-
-
 const webDevQuiz = {};
 
 webDevQuiz.score = 0;
 
-webDevQuiz.compareAnswers = (questionNumber, correctAnswer) => {
+webDevQuiz.computeScore = () => {
+    webDevQuiz.score = $('.correctAnswer').length - $('.incorrectAnswer').length;
+};
+
+webDevQuiz.resetScore = () => {
+    webDevQuiz.score = 0;
+};
+
+webDevQuiz.displayScore = numQuestions => {
+    const $userScore = webDevQuiz.score;
+    const $scoreText = `<h2>You answered ${$userScore} out of ${numQuestions} questions correctly</h2>`;
+    
+    $('.userScore').removeClass('hide').html($scoreText);
+};
+
+webDevQuiz.insertErrorMessage = questionNumber => {
+    const $errorMessage = "<p class=\"error\" role=\"alert\" tabindex=\"0\">You must select an answer</p>";
+
+    $(`p.${questionNumber}`).after($errorMessage);
+};
+
+webDevQuiz.removeErrorMessage = questionNumber => {
+    $(`p.${questionNumber} + p.error`).remove();
+};
+
+webDevQuiz.focusAndScrollToFirstErrorMessage = () => {
+    const $firstErrorMessage = $('.error:visible').first();
+    
+    if ($('.error').length) {
+        $('html, body').stop().animate({
+            scrollTop: ($firstErrorMessage.offset().top - 20)
+        }, 400, () => {
+            $firstErrorMessage.focus();
+        });
+    }
+};
+
+webDevQuiz.selectCorrectAnswer = (questionNumber, correctAnswer) => {
+    $(`div.${questionNumber}Choice${correctAnswer.toUpperCase()}`).addClass('correctAnswer');
+};
+
+webDevQuiz.compareAnswers = (questionNumber, correctAnswer, numQuestions) => {
     const $userAnswer = $(`input[name=${questionNumber}]:checked`);
     const $userAnswerDivWrapper = $(`input[name=${questionNumber}]:checked`).parent('div');
     
     if ($userAnswer.attr('value') !== correctAnswer) {
         $userAnswerDivWrapper.addClass('incorrectAnswer');
         webDevQuiz.selectCorrectAnswer(questionNumber, correctAnswer);
+        webDevQuiz.displayScore(numQuestions);
     }
-
+    
     if ($userAnswer.attr('value') === correctAnswer) {
         $userAnswerDivWrapper.removeClass('incorrectAnswer');
         webDevQuiz.selectCorrectAnswer(questionNumber, correctAnswer);
-        webDevQuiz.score = webDevQuiz.score + 1;
+        webDevQuiz.computeScore();
+        webDevQuiz.displayScore(numQuestions);
     }
-};
-
-webDevQuiz.selectCorrectAnswer = (questionNumber, correctAnswer) => {
-    $(`div.${questionNumber}Choice${correctAnswer.toUpperCase()}`).addClass('correctAnswer');
 };
 
 webDevQuiz.validateUserAnswer = (questionNumber, correctAnswer, numQuestions) => {
@@ -60,58 +93,42 @@ webDevQuiz.validateUserAnswer = (questionNumber, correctAnswer, numQuestions) =>
     
     if ($errorMessage.length > 0 && $questionAnswerLength === numQuestions) {
         webDevQuiz.removeErrorMessage(questionNumber);
-        webDevQuiz.compareAnswers(questionNumber, correctAnswer);  
-    };
-
-    if ($questionAnswerLength === numQuestions) {
-        webDevQuiz.compareAnswers(questionNumber, correctAnswer);  
-    };
-};
-
-webDevQuiz.insertErrorMessage = questionNumber => {
-    const $errorMessage = "<p class=\"error\" role=\"alert\" tabindex=\"0\">You must select an answer</p>";
-
-    $(`p.${questionNumber}`).after($errorMessage);
-};
-
-webDevQuiz.removeErrorMessage = questionNumber => {
-    $(`p.${questionNumber} + p.error`).remove();
-};
-
-webDevQuiz.focusAndScrollToFirstErrorMessage = () => {
-    const $firstErrorMessage = $('.error:visible').first();
+        webDevQuiz.compareAnswers(questionNumber, correctAnswer, numQuestions);
+    }
     
-    if ($('.error').length) {
-        $('html, body').stop().animate({
-            scrollTop: ($firstErrorMessage.offset().top - 20)
-        }, 400, () => {
-            $firstErrorMessage.focus();
-        });
+    if ($questionAnswerLength === numQuestions) {
+        webDevQuiz.compareAnswers(questionNumber, correctAnswer, numQuestions);  
     }
 };
 
-webDevQuiz.displayResults = (questionNumber, correctAnswer, numQuestions) => {
+webDevQuiz.resetQuiz = () => {
+    $('form').trigger('reset');
+    $('div').removeClass('incorrectAnswer correctAnswer');
+    $('.userScore').addClass('hide').empty();
+    webDevQuiz.resetScore();
+};
+
+webDevQuiz.processQuizActions = (questionNumber, correctAnswer, numQuestions) => {
     $("button[type='submit']").on('click submit', (event) => {
         event.preventDefault();
 
-        webDevQuiz.validateUserAnswer(questionNumber, correctAnswer, numQuestions);
+        if ($('.correctAnswer').length === numQuestions) {
+            return false;
+        } else {
+            webDevQuiz.validateUserAnswer(questionNumber, correctAnswer, numQuestions);
+        }
     });
+
+    $("button[type='reset']").on('click', webDevQuiz.resetQuiz);
 };
 
 webDevQuiz.init = () => {
-    webDevQuiz.displayResults('questionOne', 'd', 4);
-    webDevQuiz.displayResults('questionTwo', 'c', 4);
-    webDevQuiz.displayResults('questionThree', 'b', 4);
-    webDevQuiz.displayResults('questionFour', 'a', 4);
+    webDevQuiz.processQuizActions('questionOne', 'd', 4);
+    webDevQuiz.processQuizActions('questionTwo', 'c', 4);
+    webDevQuiz.processQuizActions('questionThree', 'b', 4);
+    webDevQuiz.processQuizActions('questionFour', 'a', 4);
 };
 
 $(document).ready(() => {
     webDevQuiz.init();
 });
-
-
-// Stretch Goals:
-// choose category: depending on which category the user chooses (HTML, CSS, JavaScript), their choice will show a new quiz form
-// dynamically generate quiz: dynamically generate a new quiz from an array of questions and answers and insert into the DOM
-// show only one question at a time, and show/remove question only when it's been completed and user clicks on "next question" button
-
